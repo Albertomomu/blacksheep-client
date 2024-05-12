91d4ce6470744768c4e3c00fec4e19e84e84625a
Alberto <alberto.moreno2357@gmail.com>
2024-05-12 21:53:51 +0200

    feat: added hero slider

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,9 +1,11 @@
+import './HomePage.css'
+import { Hero } from "../../components/Hero";
 import Layout from "../../components/Layout";
 
 function HomePage() {
   return (
     <Layout>
-      <div>HomePage</div>
+      <Hero />
     </Layout>
   )
 }
