ee1f53b7ae241b7447fd92f472abe90a3e3c18ba
Alberto <alberto.moreno2357@gmail.com>
2024-05-16 18:56:15 +0200

    feat: footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import logo from "../assets/images/logo.png"; // Importa el logo
+import logo from "../assets/images/logo-white.png"; // Importa el logo
 
 function Footer() {
   return (
     <footer
-      className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left mt-32">
+      className="bg-gray-950 text-center text-white lg:text-left mt-32">
       <div
         className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
         <div className="mr-12 hidden lg:block">
@@ -11,7 +11,7 @@ function Footer() {
         </div>
         {/* <!-- Social network icons container --> */}
         <div className="flex justify-center">
-          <a className="mr-6 text-neutral-600 dark:text-neutral-200">
+          <a className="mr-6 text-white dark:text-neutral-200">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
@@ -80,65 +80,63 @@ function Footer() {
       <div className="mx-6 py-10 text-center md:text-left">
         <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
           {/* <!-- TW Elements section --> */}
-          <div className="">
+          <div className="flex flex-col justify-between">
             <img src={logo} alt="" width="150px" height="auto" />
             <p>
-              Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
+              Tu tienda de confianza para gente joven con la mejor calidad del mercado al mejor precio.
             </p>
           </div>
           {/* <!-- Products section --> */}
           <div className="">
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Products
+              Productos
             </h6>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Angular</a>
+              >Todos</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >React</a>
+              >Colecciones</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Vue</a>
+              >Rebajados</a>
             </p>
             <p>
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Laravel</a>
+              >Nuevo</a>
             </p>
           </div>
           {/* <!-- Useful links section --> */}
           <div className="">
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Useful links
+              Páginas
             </h6>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Pricing</a>
+              >Home</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Settings</a>
+              >Tienda</a>
             </p>
             <p className="mb-4">
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Orders</a>
+              >Sobre nosotros</a>
             </p>
             <p>
               <a className="text-neutral-600 dark:text-neutral-200"
-              >Help</a>
+              >Contacto</a>
             </p>
           </div>
           {/* <!-- Contact section --> */}
           <div>
             <h6
               className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Contact
+              Contacto
             </h6>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -151,7 +149,7 @@ function Footer() {
                 <path
                   d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
-              New York, NY 10012, US
+              Valencia, España
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -164,7 +162,7 @@ function Footer() {
                 <path
                   d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
               </svg>
-              info@example.com
+              info@blacksheep.com
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -177,7 +175,7 @@ function Footer() {
                   d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
                   clipRule="evenodd" />
               </svg>
-              + 01 234 567 88
+              +34 603 781 700
             </p>
             <p className="flex items-center justify-center md:justify-start">
               <svg
@@ -190,7 +188,7 @@ function Footer() {
                   d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
                   clipRule="evenodd" />
               </svg>
-              + 01 234 567 89
+              +34 603 781 700
             </p>
           </div>
         </div>
