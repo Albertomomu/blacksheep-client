f6f1e6f8ddd87df5e0f5c96e38c5b04f998f0d58
Alberto <alberto.moreno2357@gmail.com>
2024-06-14 20:19:28 +0200

    fix

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -2,7 +2,7 @@ import { ProductCard } from '../components'
 
 const ProductsGrid = () => {
   return (
-    <div className='flex flex-col gap-8 justify-between w-full'>
+    <div className='flex flex-col gap-8 justify-between w-full '>
       <ProductCard />
       <ProductCard />
       <ProductCard />
