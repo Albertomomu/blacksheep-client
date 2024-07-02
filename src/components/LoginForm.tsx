1a0c726bc9e2915b7e1b649e6cbf5f0f131b6535
Alberto <alberto.moreno2357@gmail.com>
2024-07-02 22:11:26 +0200

    fix: trying

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
