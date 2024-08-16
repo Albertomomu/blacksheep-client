b2c847df45529b93212323c34006439f73d31140
Alberto <alberto.moreno2357@gmail.com>
2024-08-16 22:28:57 +0200

    fix: trying

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -12,7 +12,7 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://server.blacksheepclothing.es:3000/auth/login/', {
+      const response = await axios.post('https://149.202.43.245:3000/auth/login/', {
         email,
         password,
       }, {
