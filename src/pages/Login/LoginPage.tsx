f3a6aa4ff4a6a243f803642ee693d9c31de5a330
Alberto <alberto.moreno2357@gmail.com>
2024-08-22 18:36:32 +0200

    style: fixed login & register screens

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -7,14 +7,16 @@ import './LoginPage.css';
 const LoginPage = () => {
   return (
     <div className='flex flex-col md:flex-row items-center justify-center min-h-screen w-full'>
-    <div className='w-[100%] md:w-[30%] h-screen flex flex-col items-center justify-center'>
-      <div className='w-full'>
-        <a href="/">
-          <img src={logo} alt="" width={150} height="auto" className='absolute left-5 top-5'/>
-        </a>
-        <LoginForm />
+      <div className='w-[100%] md:w-[30%] h-screen flex flex-col  justify-between'>
+          <div className='mt-8 pl-8'>
+              <a href="/">
+                  <img src={logo} alt="" width={150} height="auto"/>
+              </a>
+          </div>
+          <div className='flex-1 flex items-center justify-center'>
+              <LoginForm />
+          </div>
       </div>
-    </div>
     <div className='w-[100%] md:w-[70%] h-screen hidden md:block'>
       <img className='login-image' src={heroImage} alt="" />
     </div>
