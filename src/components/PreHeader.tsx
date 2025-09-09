461225f19a6ffe6c20854e0cdd49d910b5d8d0f1
Alberto <alberto.moreno2357@gmail.com>
2025-09-09 22:00:26 +0200

    fixing header

--- src/components/PreHeader.tsx
+++ src/components/PreHeader.tsx
@@ -3,13 +3,13 @@ function PreHeader() {
   return (
     <div className="w-full bg-black flex justify-between text-gray-200 px-2 py-2 text-xs">
       <div>
-        Tel: +34 123 456 789
+        Tel: +34 603781700
       </div>
       <div>
-        Obten un 50% de descuento | Compra ahora
+        Obten un 20% de descuento | Compra ahora
       </div>
       <div className="hidden sm:block">
-        Eng
+        Esp
       </div>
     </div>
   )
